3c4614846285b067dead6bcc37f5ea36db56525b
Julian <lanrarias@gmail.com>
2026-06-04 01:27:58 -0600

    Backend

---
+++ plataforma_deportiva_backend/src/main/java/plataformadeportiva/plataforma_deportiva_backend/PlataformaDeportivaBackendApplication.java
@@ -0,0 +1,13 @@
+package plataformadeportiva.plataforma_deportiva_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeportivaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaDeportivaBackendApplication.class, args);
+    }
+
+}
---
+++ plataforma_deportiva_backend/src/test/java/plataformadeportiva/plataforma_deportiva_backend/PlataformaDeportivaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package plataformadeportiva.plataforma_deportiva_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeportivaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
